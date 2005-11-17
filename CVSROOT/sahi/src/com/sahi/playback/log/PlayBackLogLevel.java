b1f6d04ca688b5466329867f4b3ca1eb1de8c3f9
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2005-11-17 19:15:04 +0000

    Making playback log levels much higher than severe, so that it is independant of log.properties

--- CVSROOT/sahi/src/com/sahi/playback/log/PlayBackLogLevel.java
+++ CVSROOT/sahi/src/com/sahi/playback/log/PlayBackLogLevel.java
@@ -9,9 +9,9 @@ import java.util.logging.Level;
  */
 public class PlayBackLogLevel extends Level {
 	private static final long serialVersionUID = 7518716444395222643L;
-	public static final Level ERROR = new PlayBackLogLevel("ERROR", Level.WARNING.intValue()+2);    
-    public static final Level FAILURE = new PlayBackLogLevel("FAILURE", Level.WARNING.intValue()+1);
-    public static final Level SUCCESS = new PlayBackLogLevel("SUCCESS", Level.INFO.intValue()-1);
+	public static final Level ERROR = new PlayBackLogLevel("ERROR", Level.SEVERE.intValue()+15);    
+    public static final Level FAILURE = new PlayBackLogLevel("FAILURE", Level.SEVERE.intValue()+10);
+    public static final Level SUCCESS = new PlayBackLogLevel("SUCCESS", Level.SEVERE.intValue()+5);
     public PlayBackLogLevel(String name, int value) {
         super(name, value);
     }
