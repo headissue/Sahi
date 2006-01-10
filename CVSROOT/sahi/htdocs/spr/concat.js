67cffa9c4ad7a75ce9161a41b41b093eae0ad84d
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-01-10 07:48:50 +0000

    reverted to sahiPoint

--- CVSROOT/sahi/htdocs/spr/concat.js
+++ CVSROOT/sahi/htdocs/spr/concat.js
@@ -244,7 +244,7 @@ function sahiSimulateMouseEvent(el, type){
 	}
 }
 var sahiPointTimer;
-function sahi_point(el){
+function sahiPoint(el){
 	var d =	document.getElementById("sahi_div");
 	d.innerHTML = "<span style='color:red;font-family:verdana;font-size:20px;'>&gt;</span>";
 	d.style.position = "absolute";
