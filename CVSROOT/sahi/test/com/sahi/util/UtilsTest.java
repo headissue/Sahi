88ac269e58a21d6afd9cf8300fbe5c0d042daae0
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-04-10 19:17:48 +0000

    Escaping html brackets while displaying script

--- CVSROOT/sahi/test/com/sahi/util/UtilsTest.java
+++ CVSROOT/sahi/test/com/sahi/util/UtilsTest.java
@@ -13,4 +13,18 @@ public class UtilsTest extends TestCase {
 		assertEquals("a/b", Utils.concatPaths("a/", "/b"));
 		assertEquals("a/b", Utils.concatPaths("a", "/b"));
 	}
+	
+	
+	public void testBlankLinesDoNotCauseProblemsInNumbering() {
+		String s = "1\na\n\nbx\ny";
+		assertEquals(5, Utils.getTokens(s).size());
+		s = "1\na\n\nbx\ny\n";
+		assertEquals(5, Utils.getTokens(s).size());
+		s = "\n\n\n";
+		assertEquals(3, Utils.getTokens(s).size());
+		s = "";
+		assertEquals(1, Utils.getTokens(s).size());
+		s = "\n";
+		assertEquals(1, Utils.getTokens(s).size());
+	}	
 }
