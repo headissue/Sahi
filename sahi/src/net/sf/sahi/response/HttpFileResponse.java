d8f8968372d6a814814367d9d2e9e3a9ee7c9f7b
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-04-14 19:58:13 +0000

    Added MimeType

--- sahi/src/net/sf/sahi/response/HttpFileResponse.java
+++ sahi/src/net/sf/sahi/response/HttpFileResponse.java
@@ -1,62 +1,47 @@
 package net.sf.sahi.response;
 
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.Properties;
 
 import net.sf.sahi.util.Utils;
 
 /**
- * User: nraman
- * Date: May 15, 2005
- * Time: 10:14:34 PM
+ * User: nraman Date: May 15, 2005 Time: 10:14:34 PM
  */
 public class HttpFileResponse extends HttpResponse {
-    private String fileName;
-//    private Properties substitutions;
+	private String fileName;
+	// private Properties substitutions;
 
-    public HttpFileResponse(String fileName, Properties substitutions) {
-        this.fileName = fileName;
-    	setData(Utils.readFile(fileName));
-        if (substitutions != null) {
-            setData(substitute(new String(data()), substitutions).getBytes());
-        }
-        setHeaders();
-    }
+	public HttpFileResponse(String fileName, Properties substitutions) {
+		this.fileName = fileName;
+		setData(Utils.readFile(fileName));
+		if (substitutions != null) {
+			setData(substitute(new String(data()), substitutions).getBytes());
+		}
+		setHeaders();
+	}
 
-    static String substitute(String content, Properties substitutions) {
-        Enumeration keys = substitutions.keys();
-        while (keys.hasMoreElements()) {
-            String key = (String) keys.nextElement();
-            content = content.replaceAll("\\$" + key, substitutions.getProperty(key));
-        }
-        return content;
-    }
+	static String substitute(String content, Properties substitutions) {
+		Enumeration keys = substitutions.keys();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			content = content.replaceAll("\\$" + key, substitutions
+					.getProperty(key));
+		}
+		return content;
+	}
 
-    public HttpFileResponse(String fileName) {
-        this(fileName, null);
-    }
+	public HttpFileResponse(String fileName) {
+		this(fileName, null);
+	}
 
-    private void setHeaders() {
-        setFirstLine("HTTP/1.1 200 OK");
-        setHeader("Content-Type", getContentType(fileName));
-//        setHeader("Expires", new Date(System.currentTimeMillis()+3*60*1000).toString());
-        setHeader("Connection", "close");
-        setHeader("Content-Length", "" + data().length);
-        setRawHeaders(getRebuiltHeaderBytes());
-    }
-
-
-    private String getContentType(String fileName) {
-        if (fileName.endsWith("htm") || fileName.endsWith("html")) {
-            return "text/html";
-        } else if (fileName.endsWith("class")) {
-            return "text/plain";
-//            return "application-x/octetst";
-        }else if (fileName.endsWith("ico")) {
-        	return "image/bmp";
-        }
-        return "text/plain";
-    }
+	private void setHeaders() {
+		setFirstLine("HTTP/1.1 200 OK");
+		setHeader("Content-Type", MimeType.getMimeTypeOfFile(fileName));
+		// setHeader("Expires", new
+		// Date(System.currentTimeMillis()+3*60*1000).toString());
+		setHeader("Connection", "close");
+		setHeader("Content-Length", "" + data().length);
+		setRawHeaders(getRebuiltHeaderBytes());
+	}
 }
-
