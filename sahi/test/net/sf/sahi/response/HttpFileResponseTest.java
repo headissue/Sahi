79689e7ba3247c02d6cab0167a3d3d9f38a8b8c5
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-04-15 07:47:28 +0000

    Caching at HTTP level and file read level added.

--- sahi/test/net/sf/sahi/response/HttpFileResponseTest.java
+++ sahi/test/net/sf/sahi/response/HttpFileResponseTest.java
@@ -1,23 +1,28 @@
 package net.sf.sahi.response;
 
+import java.util.GregorianCalendar;
 import java.util.Properties;
 
 import junit.framework.TestCase;
 
 /**
- * User: nraman
- * Date: May 15, 2005
- * Time: 10:14:34 PM
+ * User: nraman Date: May 15, 2005 Time: 10:14:34 PM
  */
 public class HttpFileResponseTest extends TestCase {
-    public void testSubstitute(){
-        Properties props = new Properties();
-        props.setProperty("isRecording", "true");
-        props.setProperty("isPlaying", "false");
-        props.setProperty("sessionId", "sahi_1281210");
-        String template = " var isRecording=$isRecording;\n var isPlaying=$isPlaying;\n setCookie('$sessionId')";
-        assertEquals(" var isRecording=true;\n var isPlaying=false;\n setCookie('sahi_1281210')",
-                HttpFileResponse.substitute(template, props));
-    }
-}
+	public void testSubstitute() {
+		Properties props = new Properties();
+		props.setProperty("isRecording", "true");
+		props.setProperty("isPlaying", "false");
+		props.setProperty("sessionId", "sahi_1281210");
+		String template = " var isRecording=$isRecording;\n var isPlaying=$isPlaying;\n setCookie('$sessionId')";
+		assertEquals(
+				" var isRecording=true;\n var isPlaying=false;\n setCookie('sahi_1281210')",
+				HttpFileResponse.substitute(template, props));
+	}
 
+	public void testFormatForExpiresHeader() {
+		assertEquals("Sat, 05 May 2001 12:00:00 IST", HttpFileResponse
+				.formatForExpiresHeader(new GregorianCalendar(2001, 4, 5)
+						.getTime()));
+	}
+}
