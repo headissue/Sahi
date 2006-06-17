c8f94adde0fda815679f6ec15753bf76b2d495ee
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-06-17 19:17:04 +0000

    Removed validity option

--- sahi/test/net/sf/sahi/ssl/SSLHelperTest.java
+++ sahi/test/net/sf/sahi/ssl/SSLHelperTest.java
@@ -4,6 +4,6 @@ import junit.framework.TestCase;
 
 public class SSLHelperTest extends TestCase {
 	public void testSSLCommand() {
-		assertEquals("keytool.exe -genkey -alias www.sahi.co.in  -keypass pwd -storepass pwd -keyalg RSA -validity 10000000 -keystore filekarasta -dname \"CN=www.sahi.co.in, OU=Sahi, O=Sahi, L=Bangalore, S=Karnataka, C=IN\"" ,new SSLHelper().getSSLCommand("www.sahi.co.in", "filekarasta", "pwd", "keytool.exe").trim());
+		assertEquals("keytool.exe -genkey -alias www.sahi.co.in  -keypass pwd -storepass pwd -keyalg RSA -keystore filekarasta -dname \"CN=www.sahi.co.in, OU=Sahi, O=Sahi, L=Bangalore, S=Karnataka, C=IN\"" ,new SSLHelper().getSSLCommand("www.sahi.co.in", "filekarasta", "pwd", "keytool.exe").trim());
 	}
 }
