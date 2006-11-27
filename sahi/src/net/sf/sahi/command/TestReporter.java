ef360db99d284a4c785646fb8d7c8755670148c6
d_lewis <d_lewis@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-11-27 12:02:51 +0000

    This class logs test results

---
+++ sahi/src/net/sf/sahi/command/TestReporter.java
@@ -0,0 +1,27 @@
+package net.sf.sahi.command;
+
+import net.sf.sahi.playback.SahiScript;
+import net.sf.sahi.report.Report;
+import net.sf.sahi.request.HttpRequest;
+import net.sf.sahi.session.Session;
+
+/**
+ * @author dlewis
+ * 
+ */
+public class TestReporter {
+	public void logTestResult(HttpRequest request) {
+		Session session = request.session();
+		Report report = session.getReport();
+		report.addResult(SahiScript.stripSahiFromFunctionNames(request
+				.getParameter("msg")), request.getParameter("type"), request
+				.getParameter("debugInfo"),request
+				.getParameter("failureMsg"));
+	}
+
+	public void generateReport(HttpRequest request) {
+		Session session = request.session();
+		Report report = session.getReport();
+		report.generateReport();
+	}
+}
