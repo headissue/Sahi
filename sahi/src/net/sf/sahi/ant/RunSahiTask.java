9949151059c84394f6eab1f5d113a7dc754b2427
d_lewis <d_lewis@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-11-27 12:27:47 +0000

    Added junitReport attribute to ant task

--- sahi/src/net/sf/sahi/ant/RunSahiTask.java
+++ sahi/src/net/sf/sahi/ant/RunSahiTask.java
@@ -20,50 +20,73 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import net.sf.sahi.test.TestRunner;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
-import net.sf.sahi.test.TestRunner;
 
 public class RunSahiTask extends Task {
 	private String suite;
+
 	private String scriptsDir;
+
 	private String logDir = "";
+
+	private String junitReport;
+
 	private String browser;
+
 	private String baseURL;
+
 	private String sahiHost;
+
 	private String sahiPort;
+
 	private String failureProperty;
+
 	private String haltOnFailure;
+
 	private String stop;
+
 	private String threads = "1";
 
 	public String getBaseURL() {
 		return baseURL;
 	}
+
 	public void setBaseURL(String baseURL) {
 		this.baseURL = baseURL;
 	}
+
 	public String getBrowser() {
 		return browser;
 	}
+
 	public void setBrowser(String browser) {
 		this.browser = browser;
 	}
+
 	public String getSuite() {
 		return suite;
 	}
+
 	public void setSuite(String suite) {
 		this.suite = suite;
 	}
+
 	public String getSahiPort() {
 		return sahiPort;
 	}
+
 	public void setSahiPort(String port) {
 		this.sahiPort = port;
 	}
+
 	public String getSahiHost() {
 		return sahiHost;
 	}
+
 	public void setSahiHost(String sahiHost) {
 		this.sahiHost = sahiHost;
 	}
@@ -77,21 +100,23 @@ public class RunSahiTask extends Task {
 	}
 
 	private void stopServer() {
-			try {
-				URL url = new URL("http://"+sahiHost+":"+sahiPort+"/_s_/dyn/stopserver");
-				InputStream s = url.openStream();
-				s.close();
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-			}
+		try {
+			URL url = new URL("http://" + sahiHost + ":" + sahiPort
+					+ "/_s_/dyn/stopserver");
+			InputStream s = url.openStream();
+			s.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+		}
 	}
 
 	private void startServer() {
 		boolean isFailure = false;
 		String status = "FAILURE";
 		try {
-			TestRunner testRunner = new TestRunner(suite, browser, baseURL, logDir, sahiHost, sahiPort, threads);
+			TestRunner testRunner = new TestRunner(suite, browser, baseURL,
+					logDir, junitReport, sahiHost, sahiPort, threads);
 			status = testRunner.execute();
 			if (!"SUCCESS".equals(status)) {
 				isFailure = true;
@@ -101,7 +126,7 @@ public class RunSahiTask extends Task {
 			e.printStackTrace();
 		}
 		if (isFailure) {
-			System.out.println("STATUS:"+status);
+			System.out.println("STATUS:" + status);
 			if (failureProperty != null) {
 				getProject().setProperty(failureProperty, "true");
 			}
@@ -110,41 +135,56 @@ public class RunSahiTask extends Task {
 			}
 		}
 	}
+
 	public String getFailureProperty() {
 		return failureProperty;
 	}
+
 	public void setFailureProperty(String failureProperty) {
 		this.failureProperty = failureProperty;
 	}
+
 	public String getHaltOnFailure() {
 		return haltOnFailure;
 	}
+
 	public void setHaltOnFailure(String haltOnFailure) {
 		this.haltOnFailure = haltOnFailure;
 	}
+
 	public void setStop(String stop) {
 		this.stop = stop;
 	}
+
 	public String getThreads() {
 		return threads;
 	}
+
 	public void setThreads(String threads) {
 		this.threads = threads;
 	}
 
-    public String getScriptsDir() {
-        return scriptsDir;
-    }
+	public String getScriptsDir() {
+		return scriptsDir;
+	}
 
-    public void setScriptsDir(String scriptsDir) {
-        this.scriptsDir = scriptsDir;
-    }
+	public void setScriptsDir(String scriptsDir) {
+		this.scriptsDir = scriptsDir;
+	}
 
-    public String getLogDir() {
-        return logDir;
-    }
+	public String getLogDir() {
+		return logDir;
+	}
 
-    public void setLogDir(String logDir) {
-        this.logDir = logDir;
-    }
+	public void setLogDir(String logDir) {
+		this.logDir = logDir;
+	}
+
+	public String getJunitReport() {
+		return junitReport;
+	}
+
+	public void setJunitReport(String junitReport) {
+		this.junitReport = junitReport;
+	}
 }
