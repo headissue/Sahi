3a2b172ffedb8ab20050bce7ce1c83ce24622ed4
d_lewis <d_lewis@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2006-12-05 11:21:58 +0000

    Removed all getter methods

--- sahi/src/net/sf/sahi/ant/RunSahiTask.java
+++ sahi/src/net/sf/sahi/ant/RunSahiTask.java
@@ -16,186 +16,136 @@
 
 package net.sf.sahi.ant;
 
+import net.sf.sahi.test.TestRunner;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import net.sf.sahi.test.TestRunner;
-
-import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Task;
-
 public class RunSahiTask extends Task {
-	private String suite;
+    private String suite;
 
-	private String scriptsDir;
+    private String scriptsDir;
 
-	private String logDir = "";
+    private String logDir = "";
 
-	private String junitReport;
+    private String junitReport;
 
-	private String browser;
+    private String browser;
 
-	private String baseURL;
+    private String baseURL;
 
-	private String sahiHost;
+    private String sahiHost;
 
-	private String sahiPort;
+    private String sahiPort;
 
-	private String failureProperty;
+    private String failureProperty;
 
-	private String haltOnFailure;
+    private String haltOnFailure;
 
-	private String stop;
+    private String stop;
 
-	private String threads = "1";
+    private String threads = "1";
 
     private String browserOption;
-
-
-    public String getBrowserOption() {
-        return browserOption;
-    }
 
     public void setBrowserOption(String browserOption) {
         this.browserOption = browserOption;
     }
 
-     public String getBaseURL() {
-		return baseURL;
-	}
+    public void setBaseURL(String baseURL) {
+        this.baseURL = baseURL;
+    }
 
-	public void setBaseURL(String baseURL) {
-		this.baseURL = baseURL;
-	}
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
 
-	public String getBrowser() {
-		return browser;
-	}
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
 
-	public void setBrowser(String browser) {
-		this.browser = browser;
-	}
+    public void setSahiPort(String port) {
+        this.sahiPort = port;
+    }
 
-	public String getSuite() {
-		return suite;
-	}
+    public void setSahiHost(String sahiHost) {
+        this.sahiHost = sahiHost;
+    }
 
-	public void setSuite(String suite) {
-		this.suite = suite;
-	}
+    public void execute() throws BuildException {
+        if (stop != null) {
+            stopServer();
+            return;
+        }
+        startServer();
+    }
 
-	public String getSahiPort() {
-		return sahiPort;
-	}
+    private void stopServer() {
+        try {
+            URL url = new URL("http://" + sahiHost + ":" + sahiPort
+                    + "/_s_/dyn/stopserver");
+            InputStream s = url.openStream();
+            s.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+        }
+    }
 
-	public void setSahiPort(String port) {
-		this.sahiPort = port;
-	}
+    private void startServer() {
+        boolean isFailure = false;
+        String status = "FAILURE";
+        try {
+            TestRunner testRunner = new TestRunner(suite, browser, baseURL,
+                    logDir, junitReport, sahiHost, sahiPort, threads, browserOption);
+            status = testRunner.execute();
+            if (!"SUCCESS".equals(status)) {
+                isFailure = true;
+            }
+        } catch (Exception e) {
+            isFailure = true;
+            e.printStackTrace();
+        }
+        if (isFailure) {
+            System.out.println("STATUS:" + status);
+            if (failureProperty != null) {
+                getProject().setProperty(failureProperty, "true");
+            }
+            if ("true".equalsIgnoreCase(haltOnFailure)) {
+                throw new BuildException(status);
+            }
+        }
+    }
 
-	public String getSahiHost() {
-		return sahiHost;
-	}
+    public void setFailureProperty(String failureProperty) {
+        this.failureProperty = failureProperty;
+    }
 
-	public void setSahiHost(String sahiHost) {
-		this.sahiHost = sahiHost;
-	}
+    public void setHaltOnFailure(String haltOnFailure) {
+        this.haltOnFailure = haltOnFailure;
+    }
 
-	public void execute() throws BuildException {
-		if (stop != null) {
-			stopServer();
-			return;
-		}
-		startServer();
-	}
+    public void setStop(String stop) {
+        this.stop = stop;
+    }
 
-	private void stopServer() {
-		try {
-			URL url = new URL("http://" + sahiHost + ":" + sahiPort
-					+ "/_s_/dyn/stopserver");
-			InputStream s = url.openStream();
-			s.close();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-		}
-	}
+    public void setThreads(String threads) {
+        this.threads = threads;
+    }
 
-	private void startServer() {
-		boolean isFailure = false;
-		String status = "FAILURE";
-		try {
-			TestRunner testRunner = new TestRunner(suite, browser, baseURL,
-					logDir, junitReport, sahiHost, sahiPort, threads,browserOption);
-			status = testRunner.execute();
-			if (!"SUCCESS".equals(status)) {
-				isFailure = true;
-			}
-		} catch (Exception e) {
-			isFailure = true;
-			e.printStackTrace();
-		}
-		if (isFailure) {
-			System.out.println("STATUS:" + status);
-			if (failureProperty != null) {
-				getProject().setProperty(failureProperty, "true");
-			}
-			if ("true".equalsIgnoreCase(haltOnFailure)) {
-				throw new BuildException(status);
-			}
-		}
-	}
+    public void setScriptsDir(String scriptsDir) {
+        this.scriptsDir = scriptsDir;
+    }
 
-	public String getFailureProperty() {
-		return failureProperty;
-	}
+    public void setLogDir(String logDir) {
+        this.logDir = logDir;
+    }
 
-	public void setFailureProperty(String failureProperty) {
-		this.failureProperty = failureProperty;
-	}
-
-	public String getHaltOnFailure() {
-		return haltOnFailure;
-	}
-
-	public void setHaltOnFailure(String haltOnFailure) {
-		this.haltOnFailure = haltOnFailure;
-	}
-
-	public void setStop(String stop) {
-		this.stop = stop;
-	}
-
-	public String getThreads() {
-		return threads;
-	}
-
-	public void setThreads(String threads) {
-		this.threads = threads;
-	}
-
-	public String getScriptsDir() {
-		return scriptsDir;
-	}
-
-	public void setScriptsDir(String scriptsDir) {
-		this.scriptsDir = scriptsDir;
-	}
-
-	public String getLogDir() {
-		return logDir;
-	}
-
-	public void setLogDir(String logDir) {
-		this.logDir = logDir;
-	}
-
-	public String getJunitReport() {
-		return junitReport;
-	}
-
-	public void setJunitReport(String junitReport) {
-		this.junitReport = junitReport;
-	}
+    public void setJunitReport(String junitReport) {
+        this.junitReport = junitReport;
+    }
 }
