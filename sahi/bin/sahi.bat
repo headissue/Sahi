3f62406c54908140b4568ddbc1ca896f1fd0fa1f
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2007-12-14 11:27:09 +0000

    keeping mysql lines commented

--- sahi/bin/sahi.bat
+++ sahi/bin/sahi.bat
@@ -1,5 +1,5 @@
 SET EXT_CLASS_PATH=;
-SET EXT_CLASS_PATH=%EXT_CLASS_PATH%;..\extlib\mysql-connector-java-5.0.4-bin.jar
+#SET EXT_CLASS_PATH=%EXT_CLASS_PATH%;..\extlib\mysql-connector-java-5.0.4-bin.jar
 SET SAHI_CLASS_PATH=..\lib\sahi.jar
 SET MOZ_NO_REMOTE=1
 java -classpath %EXT_CLASS_PATH%;%SAHI_CLASS_PATH% net.sf.sahi.Proxy
--- sahi/bin/sahi.sh
+++ sahi/bin/sahi.sh
@@ -1,3 +1,5 @@
-MOZ_NO_REMOTE=1
-export MOZ_NO_REMOTE
-java -jar ../lib/sahi.jar
+SET EXT_CLASS_PATH=;
+#SET EXT_CLASS_PATH=%EXT_CLASS_PATH%:..\extlib\mysql-connector-java-5.0.4-bin.jar
+SET SAHI_CLASS_PATH=..\lib\sahi.jar
+SET MOZ_NO_REMOTE=1
+java -classpath %EXT_CLASS_PATH%;%SAHI_CLASS_PATH% net.sf.sahi.Proxy
