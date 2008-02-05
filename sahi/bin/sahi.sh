4a059cda89b3544f66a9e85d16aba3b59f363d20
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2008-02-05 04:20:15 +0000

    semicolon changed to colon.

--- sahi/bin/sahi.sh
+++ sahi/bin/sahi.sh
@@ -1,6 +1,6 @@
 #if this does not work, try to run dos2unix on this file, or delete and reintroduce newlines.
-SET EXT_CLASS_PATH=;
-#SET EXT_CLASS_PATH=%EXT_CLASS_PATH%:..\extlib\mysql-connector-java-5.0.4-bin.jar
-SET SAHI_CLASS_PATH=..\lib\sahi.jar
-SET MOZ_NO_REMOTE=1
-java -classpath %EXT_CLASS_PATH%;%SAHI_CLASS_PATH% net.sf.sahi.Proxy
+EXT_CLASS_PATH=;
+#EXT_CLASS_PATH=%EXT_CLASS_PATH%:..\extlib\mysql-connector-java-5.0.4-bin.jar
+SAHI_CLASS_PATH=..\lib\sahi.jar
+MOZ_NO_REMOTE=1
+java -classpath %EXT_CLASS_PATH%:%SAHI_CLASS_PATH% net.sf.sahi.Proxy
