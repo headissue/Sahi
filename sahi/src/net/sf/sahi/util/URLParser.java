e4e8785220b1125d3a297bbf1b0f68efbe099ed5
StringyLow <StringyLow@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2008-06-26 23:38:19 +0000

    finalized function paramenters

--- sahi/src/net/sf/sahi/util/URLParser.java
+++ sahi/src/net/sf/sahi/util/URLParser.java
@@ -22,43 +22,43 @@ import net.sf.sahi.config.Configuration;
 import net.sf.sahi.command.Command;
 
 public class URLParser {
-    public static String logFileNamefromURI(String uri) {
+    public static String logFileNamefromURI(final String uri) {
         String fileName = getRelativeLogFile(uri);
-        if ("".equals(fileName)) return "";
+        if ("".equals(fileName)) { return "";}
         return Configuration.appendLogsRoot(fileName);
     }
 
-    static String getRelativeLogFile(String uri) {
+    static String getRelativeLogFile(final String uri) {
         String fileName = uri.substring(uri.indexOf(Command.LOG_VIEW) + Command.LOG_VIEW.length());
-        while (fileName.startsWith("/")) fileName = fileName.substring(1);
+        while (fileName.startsWith("/")) {fileName = fileName.substring(1);}
         return fileName;
     }
 
-    public static String fileNamefromURI(String uri) {
+    public static String fileNamefromURI(final String uri) {
         return Utils.concatPaths(Configuration.getHtdocsRoot(), uri
                 .substring(uri.indexOf("_s_/") + 4));
     }
 
-    public static String scriptFileNamefromURI(String uri, String token) {
+    public static String scriptFileNamefromURI(final String uri, final String token) {
         int endIndex = uri.indexOf("?");
         endIndex = endIndex == -1 ? uri.length() : endIndex;
         String fileName = uri.substring(uri.lastIndexOf(token) + token.length(), endIndex);
         return Utils.concatPaths(Configuration.getScriptRoots()[0], fileName); //TODO FIX ME
     }
 
-    public static String getCommandFromUri(String uri) {
+    public static String getCommandFromUri(final String uri) {
         int ix1 = uri.indexOf("/dyn/");
         if (ix1 == -1) return null;
         ix1 = ix1 + 5;
         int ix2 = uri.indexOf("/", ix1);
         int ix3 = uri.indexOf("?", ix1);
         int endIx = -1;
-        if (ix2 > -1 && ix3 == -1) endIx = ix2;
-        else if (ix3 > -1 && ix2 == -1) endIx = ix3;
+        if (ix2 > -1 && ix3 == -1) {endIx = ix2;}
+        else if (ix3 > -1 && ix2 == -1) {endIx = ix3;}
         else {
             endIx = ix3 < ix2 ? ix3 : ix2;
         }
-        if (endIx == -1) endIx = uri.length();
+        if (endIx == -1) {endIx = uri.length();}
         return uri.substring(ix1, endIx);
     }
 
