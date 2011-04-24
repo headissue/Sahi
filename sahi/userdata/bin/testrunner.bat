425e137f9e403de79f79c94952d2e84aea52b2b8
narayanraman <narayanraman@93a2d107-5d11-49df-9b07-1379baa6f0ba>
2011-04-24 17:19:41 +0000

    Single Session implementation

--- sahi/userdata/bin/testrunner.bat
+++ sahi/userdata/bin/testrunner.bat
@@ -7,7 +7,8 @@ SET USERDATA_DIR=..\
 SET SCRIPTS_PATH=scripts
 SET START_URL=%2
 SET THREADS=5
-java -cp %SAHI_HOME%\lib\ant-sahi.jar net.sf.sahi.test.TestRunner -test %SCRIPTS_PATH%/%1 -browserType %3 -baseURL "%START_URL%" -host localhost -port 9999 -threads %THREADS%
+SET SINGLE_SESSION=false
+java -cp %SAHI_HOME%\lib\ant-sahi.jar net.sf.sahi.test.TestRunner -test %SCRIPTS_PATH%/%1 -browserType %3 -baseURL "%START_URL%" -host localhost -port 9999 -threads %THREADS% -useSingleSession %SINGLE_SESSION%
 goto :EOF
 
 :ERROR
