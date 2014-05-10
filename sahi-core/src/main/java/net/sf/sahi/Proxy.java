5e7988b3ec4ba0777ff0b541ebd9eb0f19e10f3f
Andreas Worm <andreas.worm@headissue.com>
2014-05-10 17:56:27 +0200

    proxy now accepts "SAHI_HOME" as the one and only parameter

--- sahi-core/src/main/java/net/sf/sahi/Proxy.java
+++ sahi-core/src/main/java/net/sf/sahi/Proxy.java
@@ -71,8 +71,10 @@ public class Proxy {
   }
 
   public static void main(String[] args) {
-    if (args.length > 0) {
+    if (args.length == 2) {
       Configuration.init(args[0], args[1]);
+    } else if (args.length == 1) {
+      Configuration.init(args[0]);
     } else {
       Configuration.init();
     }
