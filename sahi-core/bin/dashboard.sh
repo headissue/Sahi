68f6ff6aa17351b6187006f7d60325547e6ce587
globalworming <andreas.worm@headissue.com>
2014-06-17 09:43:40 +0200

    default userdata dir

--- sahi-core/bin/dashboard.sh
+++ sahi-core/bin/dashboard.sh
@@ -4,5 +4,10 @@ if [ ! $SAHI_HOME ]; then
   export SAHI_HOME=..
 fi
 
+if [ ! $SAHI_USERDATA_DIR ]
+then
+	export SAHI_USERDATA_DIR=$SAHI_HOME/userdata
+fi
+
 SAHI_CLASS_PATH=$SAHI_HOME/lib/sahi-jar-with-dependencies.jar
-java -classpath $SAHI_EXT_CLASS_PATH:$SAHI_CLASS_PATH net.sf.sahi.ui.Dashboard "${SAHI_HOME}" "${1}"
+java -classpath $SAHI_EXT_CLASS_PATH:$SAHI_CLASS_PATH net.sf.sahi.ui.Dashboard "${SAHI_HOME}" "${SAHI_USERDATA_DIR}"
