28da914cd4ff04e3fe936d887626070d20620c2d
globalworming <andreas.worm@headissue.com>
2014-09-10 11:22:00 +0200

    last changes for release

--- sahi-core/bin/testrunner.sh
+++ sahi-core/bin/testrunner.sh
@@ -10,7 +10,7 @@ esac
 US=`dirname $0`;
 US=`(cd $US; pwd)`;
 
-java -cp $US/../../sahi-test-runner/target/sahi-test-runner-4.4-jar-with-dependencies.jar  \
+java -cp $US/../../sahi-test-runner/target/sahi-test-runner-4.4.0-H7E4-jar-with-dependencies.jar  \
 net.sf.sahi.test.TestRunner \
 -test $script -browserType phantomjs \
 -baseURL http://sahi.example.com/_s_/dyn/Driver_initialized \
