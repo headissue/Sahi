d2de4e400c2bc291b9174972cd8f966dd38881fe
Andreas Worm <aw_github@headissue.com>
2015-12-03 15:18:25 +0100

    Update dashboard.sh
    to allow origin headers, http://stackoverflow.com/questions/8335501/does-httpurlconnection-censor-some-headers-notably-origin

--- sahi-core/bin/dashboard.sh
+++ sahi-core/bin/dashboard.sh
@@ -12,4 +12,4 @@ then
 fi
 
 SAHI_CLASS_PATH=$SAHI_HOME/lib/sahi-jar-with-dependencies.jar
-java -classpath $SAHI_EXT_CLASS_PATH:$SAHI_CLASS_PATH net.sf.sahi.ui.Dashboard "${SAHI_HOME}" "${SAHI_USERDATA_DIR}"
+java -Dsun.net.http.allowRestrictedHeaders=true -classpath $SAHI_EXT_CLASS_PATH:$SAHI_CLASS_PATH net.sf.sahi.ui.Dashboard "${SAHI_HOME}" "${SAHI_USERDATA_DIR}"
